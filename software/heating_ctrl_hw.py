a8176ce99b908144cd947d7bf512d17c5d3e1e6a
Christian Eisenhut (Home-Office) <christian.eisenhut@oertli-instruments.com>
2016-10-17 14:47:26 +0200

    adapt hardware drivers
    Signed-off-by: Christian Eisenhut (Home-Office) <christian.eisenhut@oertli-instruments.com>

--- software/heating_ctrl_hw.py
+++ software/heating_ctrl_hw.py
@@ -24,15 +24,11 @@ class CtrlHardware():
 	#sensor_id: 0..15
 	def readTemp (self, sensor_id):
 		if sensor_id >= 0 and sensor_id < 8:
-			#set output-latch of GPB3 low, GPB4 high
-			mux_byte = format(16+sensor_id, '#04x')
-			os.system('i2cset -y 1 0x24 0x15 '+mux_byte)
-		elif sensor_id >7 and sensor_id < 16:
-			#set output-latch of GPB3 high, GPB4 low
+			#set output-latch of GPB3 low
 			mux_byte = format(sensor_id, '#04x')
 			os.system('i2cset -y 1 0x24 0x15 '+mux_byte)
-		#set pins 0..4 of register B as output
-		os.system('i2cset -y 1 0x24 0x01 0xE0')
+		#set pins 0..3 of register B as output
+		os.system('i2cset -y 1 0x24 0x01 0xF0')
 		time.sleep (0.2)
 		adc_result = self.spi.xfer([0x01, 0x80, 0x00])
 		value = 256*adc_result[1] + adc_result[2]
@@ -41,13 +37,13 @@ class CtrlHardware():
 
 	def initOutputs (self):
 		self._output = 0xff
-		os.system('i2cset -y 1 0x20 0x15 0xFF')
-		os.system('i2cset -y 1 0x20 0x01 0x00')  # set pins 0..7 of register B as output
+		os.system('i2cset -y 1 0x20 0x15 0x00')
+		os.system('i2cset -y 1 0x20 0x01 0x00')
 
 	def setOutput (self, output):
 		self._output = (~output + 256)
 		os.system('i2cset -y 1 0x20 0x15 ' + format(self._output, '#04x'))
-		os.system('i2cset -y 1 0x20 0x01 0x00')  #set pins 0..4 of register B as output
+		os.system('i2cset -y 1 0x20 0x01 0x00')
 
 	def changeOutput (self, pin=0, state=0):
 		"""
@@ -61,7 +57,7 @@ class CtrlHardware():
 		else:
 			self._output = self._output & ((~(2**pin))+ 256)
 		os.system('i2cset -y 1 0x20 0x15 ' + format(self._output, '#04x'))
-		os.system('i2cset -y 1 0x20 0x01 0x00')  #set pins 0..4 of register B as output
+		os.system('i2cset -y 1 0x20 0x01 0x00')
 
 
 	
--- software/solar_heating_ctrl.py
+++ software/solar_heating_ctrl.py
@@ -77,21 +77,16 @@ while True:
     T6 = hw.readTemp(5)
     T7 = hw.readTemp(6)
     T8 = hw.readTemp(7)
-    T9 = hw.readTemp(8)
-    T10 = hw.readTemp(9)
-    T11 = hw.readTemp(10)
 
-    ReadTime = 2.2 #0.2 seconds for each read
+    ReadTime = 1.6 #0.2 seconds for each read
     # TODO: split sampling and reading
 
-    TempLog = "T1:%2.1f,T2:%2.1f,T3:%2.1f,T4:%2.1f,T5:%2.1f,T6:%2.1f,T7:%2.1f,T8:%2.1f,T9:%2.1f,T10:%2.1f,T11:%2.1f" % (
-    T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11)
-    #print("T1 = %2.1f, T2 = %2.1f, T3 = %2.1f, T4 = %2.1f, T5 = %2.1f, T6 = %2.1f, T7 = %2.1f" % (T1, T2, T3, T4, T5, T6, T7))
-    #print("T8 = %2.1f, T9 = %2.1f, T10 = %2.1f, T11 = %2.1f" % (T8, T9, T10, T11))
+    TempLog = "T1:%2.1f,T2:%2.1f,T3:%2.1f,T4:%2.1f,T5:%2.1f,T6:%2.1f,T7:%2.1f,T8:%2.1f" % (
+    T1, T2, T3, T4, T5, T6, T7, T8)
 
     # mean-temperature:
-    StorageMeanTemp = (T1 * 30 + T2 * 5 + T3 * 5 + T4 * 10 + T5 * 20 + T6 * 30) / 100
-    #print("StorageMeanTemp = %2.1f" % StorageMeanTemp)
+    StorageMeanTemp = (T1 * 25 + T2 * 25 + T3 * 25 + T4 * 25) / 100
+
     StorageMeanTempLog = "StorageMeanTemp:%2.1f" % (StorageMeanTemp)
 
     PostDataTime -= SampleTime
@@ -105,22 +100,21 @@ while True:
     # solar-control:
     # ========================================
 
-
-    if (StorageMeanTemp > 80):  # activ cooling
-        SolarPumpRunning = True
-        runSolarPump()
-        SwitchOnTime = 0
-        #print("solar-pump running (cooling/heating)")
-    else:
-        if SolarPumpRunning:
-            SwitchOnTime += SampleTime
-            if (T9 > T8) and (SwitchOnTime > 300):
-                SolarPumpRunning = False
-                stopSolarPump()
-                SwitchOnTime = 0
-        elif (T7 > (T6 + 4)):
-            SolarPumpRunning = True
-            runSolarPump()
+    # if (StorageMeanTemp > 80):  # activ cooling
+    #     SolarPumpRunning = True
+    #     runSolarPump()
+    #     SwitchOnTime = 0
+    #     #print("solar-pump running (cooling/heating)")
+    # else:
+    #     if SolarPumpRunning:
+    #         SwitchOnTime += SampleTime
+    #         if (T9 > T8) and (SwitchOnTime > 300):
+    #             SolarPumpRunning = False
+    #             stopSolarPump()
+    #             SwitchOnTime = 0
+    #     elif (T7 > (T6 + 4)):
+    #         SolarPumpRunning = True
+    #         runSolarPump()
 
     #if (SolarPumpRunning):
         #print("solar-pump running")
@@ -130,53 +124,53 @@ while True:
     # heating-control:
     # ========================================
 
-    HeatTempSetpoint = emon.readHeatingTempSetpoint()
-
-    if (HeatingState == 0):  #heating off: setpoint = 0, temperature-mixer to lowest position
-        HeatingPumpRunning = False
-        if (DecreaseTempTime == 0):
-            HeatingState = 1
-    elif (HeatingState == 1):  #heating idle: heating-system off, waiting for setpoint > 0
-        HeatingPumpRunning = False
-        if (HeatTempSetpoint > 0):
-            HeatingState = 2
-            ControlSampleTime = 0  # start control immediately
-    elif (HeatingState == 2): #heating-control: setpoint > 0, pump running, closed-loop temperature control
-        HeatingPumpRunning = True
-        ControlSampleTime -= SampleTime
-        if (ControlSampleTime <= 0):
-            ControlSampleTime = HeatControlSampleTime
-            ControlError = HeatTempSetpoint - T10
-            if (ControlError > 3):
-                IncreaseTempTime = ControlError/3
-            elif (ControlError < 3):
-                DecreaseTempTime = -ControlError/2
-        if (HeatTempSetpoint == 0):
-            HeatingState = 0
-            DecreaseTempTime = 200
-    else:
-        HeatingPumpRunning = False
-        DecreaseTempTime = 200
-        HeatingState = 0
-
-
-    if (HeatingPumpRunning):
-        runHeatingPump()
-    else:
-        stopHeatingPump()
-
-    if (DecreaseTempTime > 0):
-        DecreaseTempTime -= SampleTime
-        decreaseHeatingTemp()
-        IncreaseTempTime = 0
-    elif (IncreaseTempTime > 0):
-        IncreaseTempTime -= SampleTime
-        increaseHeatingTemp()
-        DecreaseTempTime = 0
-    else:
-        freezeHeatingTemp()
-        DecreaseTempTime = 0
-        IncreaseTempTime = 0
+    # HeatTempSetpoint = emon.readHeatingTempSetpoint()
+    #
+    # if (HeatingState == 0):  #heating off: setpoint = 0, temperature-mixer to lowest position
+    #     HeatingPumpRunning = False
+    #     if (DecreaseTempTime == 0):
+    #         HeatingState = 1
+    # elif (HeatingState == 1):  #heating idle: heating-system off, waiting for setpoint > 0
+    #     HeatingPumpRunning = False
+    #     if (HeatTempSetpoint > 0):
+    #         HeatingState = 2
+    #         ControlSampleTime = 0  # start control immediately
+    # elif (HeatingState == 2): #heating-control: setpoint > 0, pump running, closed-loop temperature control
+    #     HeatingPumpRunning = True
+    #     ControlSampleTime -= SampleTime
+    #     if (ControlSampleTime <= 0):
+    #         ControlSampleTime = HeatControlSampleTime
+    #         ControlError = HeatTempSetpoint - T10
+    #         if (ControlError > 3):
+    #             IncreaseTempTime = ControlError/3
+    #         elif (ControlError < 3):
+    #             DecreaseTempTime = -ControlError/2
+    #     if (HeatTempSetpoint == 0):
+    #         HeatingState = 0
+    #         DecreaseTempTime = 200
+    # else:
+    #     HeatingPumpRunning = False
+    #     DecreaseTempTime = 200
+    #     HeatingState = 0
+    #
+    #
+    # if (HeatingPumpRunning):
+    #     runHeatingPump()
+    # else:
+    #     stopHeatingPump()
+    #
+    # if (DecreaseTempTime > 0):
+    #     DecreaseTempTime -= SampleTime
+    #     decreaseHeatingTemp()
+    #     IncreaseTempTime = 0
+    # elif (IncreaseTempTime > 0):
+    #     IncreaseTempTime -= SampleTime
+    #     increaseHeatingTemp()
+    #     DecreaseTempTime = 0
+    # else:
+    #     freezeHeatingTemp()
+    #     DecreaseTempTime = 0
+    #     IncreaseTempTime = 0
 
     #print ("HeatingState = ", HeatingState)
     #print ("DecreaseTempTime = ", DecreaseTempTime)
