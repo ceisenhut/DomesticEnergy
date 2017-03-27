3815b7170c951823c296a148c0884d71d72a3c19
Christian Eisenhut <cei@NB-29.oertli-ch.intra>
2017-03-27 07:05:37 +0200

    correct indentation levels
    Signed-off-by: Christian Eisenhut <cei@NB-29.oertli-ch.intra>

--- software/heating_ctrl_hw_test.py
+++ software/heating_ctrl_hw_test.py
@@ -8,21 +8,15 @@ try:
     hw = CtrlHardware()
 
     try:
-            DutyCycle = (int) (sys.argv[1])
+        DutyCycle = (int) (sys.argv[1])
     except:
-            DutyCycle = 0
+        DutyCycle = 0
 
     if (DutyCycle <= 100 and DutyCycle >= 0):
-            print (DutyCycle)
+        print (DutyCycle)
     else:
-            print ('else-path')
-            DutyCycle = 0
-
-
-    #hw.initOutputs()
-    #hw.changeOutput(pin=6, state=1)
-    #hw.changeOutput(pin=5, state=1)
-    #hw.setOutput(0x80)
+        print ('else-path')
+        DutyCycle = 0
 
 	ElectricHeatPWM = hw.initPWM(36, 1000)
 	hw.setPWM(ElectricHeatPWM, DutyCycle)
