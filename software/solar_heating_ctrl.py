648ff972a44ed9219ed14df0ec81ecd58eb7d0b4
Christian Eisenhut <cei@NB-29.oertli-ch.intra>
2017-03-29 16:03:45 +0200

    add control of electrical heating
    Signed-off-by: Christian Eisenhut <cei@NB-29.oertli-ch.intra>

--- software/solar_heating_ctrl.py
+++ software/solar_heating_ctrl.py
@@ -61,163 +61,173 @@ ChargeIntegral = 0
 T3_entry = 0
 T2_entry = 0
 
-PWM_PinElectricHeating = 36
+PWM_PinElectricHeating = 37
 PWM_Frequency = 1000
 ActualDutyCycle = 0
 
 Setpoint_ChargingState = 0
 
 #========================================
-hw.initOutputs()
-setValves4Idle()
-time.sleep(45)
-hw.initOutputs()
-#hw.initPWM(PWM_PinElectricHeating)
+try:
+    hw.initOutputs()
+    setValves4Idle()
+    time.sleep(45)
+    hw.initOutputs()
 
-while True:
-    T1 = hw.readTemp(0)
-    T2 = hw.readTemp(1)
-    T3 = hw.readTemp(2)
-    T4 = hw.readTemp(3)
-    T5 = hw.readTemp(4)
-    T6 = hw.readTemp(5)
-    T7 = hw.readTemp(6)
-    T8 = hw.readTemp(7)
+    ElectricHeatPWM = hw.initPWM(PWM_PinElectricHeating, PWM_Frequency)
 
-    ReadTime = 1.6 #0.2 seconds for each read
-    # TODO: split sampling and reading
+    while True:
+        T1 = hw.readTemp(0)
+        T2 = hw.readTemp(1)
+        T3 = hw.readTemp(2)
+        T4 = hw.readTemp(3)
+        T5 = hw.readTemp(4)
+        T6 = hw.readTemp(5)
+        T7 = hw.readTemp(6)
+        T8 = hw.readTemp(7)
 
-    TempLog = "T1:%2.1f,T2:%2.1f,T3:%2.1f,T4:%2.1f,T5:%2.1f,T6:%2.1f,T7:%2.1f,T8:%2.1f" % (
-    T1, T2, T3, T4, T5, T6, T7, T8)
+        ReadTime = 1.6 #0.2 seconds for each read
+        # TODO: split sampling and reading
 
-    # mean-temperature:
-    StorageMeanTemp = (T1 * 25 + T2 * 25 + T3 * 25 + T4 * 25) / 100
+        TempLog = "T1:%2.1f,T2:%2.1f,T3:%2.1f,T4:%2.1f,T5:%2.1f,T6:%2.1f,T7:%2.1f,T8:%2.1f" % (
+        T1, T2, T3, T4, T5, T6, T7, T8)
 
-    StorageMeanTempLog = "StorageMeanTemp:%2.1f" % (StorageMeanTemp)
+        # mean-temperature:
+        StorageMeanTemp = (T1 * 25 + T2 * 25 + T3 * 25 + T4 * 25) / 100
 
-    Charging_State_Log = "ChargingState:%2.1f" % (Charging_State)
+        StorageMeanTempLog = "StorageMeanTemp:%2.1f" % (StorageMeanTemp)
 
-    ElectricalHeatingDutyCycleLog = "HeatingDutyCycle:%2.1f" % (ActualDutyCycle)
+        Charging_State_Log = "ChargingState:%2.1f" % (Charging_State)
 
-    PostDataTime -= SampleTime
-
-
-    if (PostDataTime<=0):
-        PostDataTime = 30
-        try:
-            emon.postData(TempLog, 1)
-            emon.postData(StorageMeanTempLog, 1)
-            emon.postData(Charging_State_Log, 1)
-            emon.postData(ElectricalHeatingDutyCycleLog, 1)
-        except:
-            print("local-server not accessible")
-        try:
-            emon.postDataRemoteServer(TempLog, 1)
-            emon.postDataRemoteServer(Charging_State_Log, 1)
-        except:
-            print("remote-server not accessible")
-
-        # ===========================================
-        # Control of electrical heating
-        ActualGridPower = emon.readTotalElectricalPower()
-        ActualDutyCycle = ActualDutyCycle - (ActualGridPower / 50)
-
-        if (ActualDutyCycle > 100):
-            ActualDutyCycle = 100
-        elif (ActualDutyCycle < 0):
-            ActualDutyCycle = 0
-
-        if (T1 > 85):
-            ActualDutyCycle = 0
-
-        # hw.setPWM(PWM_PinElectricHeating, PWM_Frequency, ActualDutyCycle)
+        PostDataTime -= SampleTime
 
 
-        NewChargingStateSetpoint = emon.readChargingStateSetpoint()
-        if (Setpoint_ChargingState != NewChargingStateSetpoint) and (NewChargingStateSetpoint != -1):
-            Setpoint_ChargingState = NewChargingStateSetpoint
-            Charging_State = Setpoint_ChargingState
+        if (PostDataTime<=0):
+            PostDataTime = 30
 
-    # ========================================
-    # charging-control:
-    # ========================================
-    if (StateCtrlTimeout <= 0):
-        if (Charging_State == State_SetValves4Idle):
-            Charging_State = State_Idle
-            ChargeIntegral = 0
-            hw.initOutputs()
-            StateCtrlTimeout = 30
-        elif (Charging_State == State_Idle):
-            if (T2 > 65):
-                Charging_State = State_SetValves4Charging
-                setValves4Charging()
-            StateCtrlTimeout = 45
-        elif (Charging_State == State_SetValves4Charging):
-            Charging_State = State_Charging
-            hw.initOutputs()
-            runChargePump()
-            StateCtrlTimeout = 30
-        elif (Charging_State == State_Charging):
-            StateCtrlTimeout = 30
-            if (T2 < 60):
-                Charging_State = State_ChargingIdle
-                stopChargePump()
-            if (T2 > 75):  # if T2 exceeds 75 degrees, valves may not be set correctly for charging
-                Charging_State = State_SetValves4Charging
-                setValves4Charging()
+            # ===========================================
+            # Control of electrical heating
+            # ===========================================
+            ActualGridPower = emon.readTotalElectricalPower()
+            ActualDutyCycle = ActualDutyCycle - (ActualGridPower / 50)
+
+            if (ActualDutyCycle > 100):
+                ActualDutyCycle = 100
+            elif (ActualDutyCycle < 0):
+                ActualDutyCycle = 0
+
+            if (T1 > 85):
+                ActualDutyCycle = 0
+
+            hw.setPWM(ElectricHeatPWM, ActualDutyCycle)
+
+            ElectricalHeatingDutyCycleLog = "DutyCycleElectricHeating:%3.0f" % ActualDutyCycle
+
+            try:
+                emon.postData(TempLog, 1)
+                emon.postData(StorageMeanTempLog, 1)
+                emon.postData(Charging_State_Log, 1)
+                emon.postData(ElectricalHeatingDutyCycleLog, 1)
+            except:
+                print("local-server not accessible")
+            try:
+                emon.postDataRemoteServer(TempLog, 1)
+                emon.postDataRemoteServer(Charging_State_Log, 1)
+                emon.postDataRemoteServer(ElectricalHeatingDutyCycleLog, 1)
+            except:
+                print("remote-server not accessible")
+
+            # =======================================================================
+            # Allow changing of charging state via emoncms and ChargingStateSetpoint
+            # =======================================================================
+            NewChargingStateSetpoint = emon.readChargingStateSetpoint()
+            if (Setpoint_ChargingState != NewChargingStateSetpoint) and (NewChargingStateSetpoint != -1):
+                Setpoint_ChargingState = NewChargingStateSetpoint
+                Charging_State = Setpoint_ChargingState
+
+        # ========================================
+        # charging-control:
+        # ========================================
+        if (StateCtrlTimeout <= 0):
+            if (Charging_State == State_SetValves4Idle):
+                Charging_State = State_Idle
+                ChargeIntegral = 0
+                hw.initOutputs()
+                StateCtrlTimeout = 30
+            elif (Charging_State == State_Idle):
+                if (T2 > 65):
+                    Charging_State = State_SetValves4Charging
+                    setValves4Charging()
                 StateCtrlTimeout = 45
-        elif (Charging_State == State_ChargingIdle):
-            if (T2 > 65):
+            elif (Charging_State == State_SetValves4Charging):
                 Charging_State = State_Charging
+                hw.initOutputs()
                 runChargePump()
-            if (T2 < 45):
-                Charging_State = State_SetValves4Discharging
-                setValves4Discharging()
-            if (T2 > 75):  # if T2 exceeds 75 degrees, valves may not be set correctly for charging
-                Charging_State = State_SetValves4Charging
-                setValves4Charging()
-            StateCtrlTimeout = 45
-        elif (Charging_State == State_SetValves4Discharging):
-            Charging_State = State_Discharging
-            hw.initOutputs()
-            runChargePump()
-            StateCtrlTimeout = 60
-            T3_entry = T3
-        elif (Charging_State == State_Discharging):
-            if ((T3>(T3_entry+2)) or (T7 < (T2+2))):
-                Charging_State = State_DischargingIdle
-                stopChargePump()
-                T2_entry = T2
-            if (T2 > 65):
-                Charging_State = State_SetValves4Charging
-                setValves4Charging()
-            if (T2 < 23):
-                Charging_State = State_SetValves4Idle
-                setValves4Idle()
-            StateCtrlTimeout = 60
-        elif (Charging_State == State_DischargingIdle):
-            if (T2<(T2_entry -5)):
+                StateCtrlTimeout = 30
+            elif (Charging_State == State_Charging):
+                StateCtrlTimeout = 30
+                if (T2 < 60):
+                    Charging_State = State_ChargingIdle
+                    stopChargePump()
+                if (T2 > 75):  # if T2 exceeds 75 degrees, valves may not be set correctly for charging
+                    Charging_State = State_SetValves4Charging
+                    setValves4Charging()
+                    StateCtrlTimeout = 45
+            elif (Charging_State == State_ChargingIdle):
+                if (T2 > 65):
+                    Charging_State = State_Charging
+                    runChargePump()
+                if (T2 < 45):
+                    Charging_State = State_SetValves4Discharging
+                    setValves4Discharging()
+                if (T2 > 75):  # if T2 exceeds 75 degrees, valves may not be set correctly for charging
+                    Charging_State = State_SetValves4Charging
+                    setValves4Charging()
+                StateCtrlTimeout = 45
+            elif (Charging_State == State_SetValves4Discharging):
                 Charging_State = State_Discharging
+                hw.initOutputs()
                 runChargePump()
+                StateCtrlTimeout = 60
                 T3_entry = T3
-            if (T2 < 23):
-                Charging_State = State_SetValves4Idle
+            elif (Charging_State == State_Discharging):
+                if ((T3>(T3_entry+2)) or (T7 < (T2+2))):
+                    Charging_State = State_DischargingIdle
+                    stopChargePump()
+                    T2_entry = T2
+                if (T2 > 65):
+                    Charging_State = State_SetValves4Charging
+                    setValves4Charging()
+                if (T2 < 23):
+                    Charging_State = State_SetValves4Idle
+                    setValves4Idle()
+                StateCtrlTimeout = 60
+            elif (Charging_State == State_DischargingIdle):
+                if (T2<(T2_entry -5)):
+                    Charging_State = State_Discharging
+                    runChargePump()
+                    T3_entry = T3
+                if (T2 < 23):
+                    Charging_State = State_SetValves4Idle
+                    setValves4Idle()
+                if (T2 > 65):
+                    Charging_State = State_SetValves4Charging
+                    setValves4Charging()
+                StateCtrlTimeout = 60
+            else:
+                hw.initOutputs()
                 setValves4Idle()
-            if (T2 > 65):
-                Charging_State = State_SetValves4Charging
-                setValves4Charging()
-            StateCtrlTimeout = 60
+                Charging_State = State_Idle
+                StateCtrlTimeout = 45
+
+        StateCtrlTimeout -= SampleTime
+
+
+        # ========================================
+        if ((SampleTime-ReadTime)>0):
+            time.sleep(SampleTime-ReadTime)   # TODO: split sampling and reading
         else:
-            hw.initOutputs()
-            setValves4Idle()
-            Charging_State = State_Idle
-            StateCtrlTimeout = 45
+            time.sleep(SampleTime)
 
-    StateCtrlTimeout -= SampleTime
-
-
-    # ========================================
-    if ((SampleTime-ReadTime)>0):
-        time.sleep(SampleTime-ReadTime)   # TODO: split sampling and reading
-    else:
-        time.sleep(SampleTime)
+finally:
+    hw.cleanupGPIO()
